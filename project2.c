d41075c36301506568c51cb14746639cec16433a
Parker Brandt <parker.brandt101@gmail.com>
2021-04-22 19:06:51 +0000

    Added function prototypes and created a more detailed outline of the main function

--- project2.c
+++ project2.c
@@ -6,17 +6,33 @@
 #define STDERR 2
 
 
+// Structs
+struct Process
+{
+	char* processName;
+	long size;
+};
+typedef struct Proccess Process;
 
+
+// Function Prototypes
 char* readline(int fd);
 char** split(char* str, char split);
 
+Process* firstFit();
+Process* bestFit();
+Process* worstFit();
+Process* nextFit();
 
+
+// Main Function
 int main(int argc, char** argv)
 {
 	// Variables
 	char* inCommand, scriptFilename;
-	int totalMemory;
+	unsigned long int totalMemory;
 
+	Process* memory;
 
 	// Check command-line arguments
 	if(argc != 4)
@@ -35,6 +51,31 @@ int main(int argc, char** argv)
 	dprintf(STDERR, "Total memory: %d\n", totalMemory);
 	dprintf(STDERR, "Script filename: %s\n", scriptFilename);
 
+	// Allocate size to the array
+	memory = (Process*)malloc(totalMemory * sizeof(Process));
 	
+	// Check to see what command was issued
+	if(strcmp(inCommand, "BESTFIT") == 0)
+	{
+
+	}
+	else if(strcmp(inCommand, "FIRSTFIT") == 0)
+	{
+
+	}
+	else if(strcmp(inCommand, "NEXTFIT") == 0)
+	{
+
+	}
+	else if(strcmp(inCommand, "WORSTFIT") == 0)
+	{
+
+	}
+	else
+	{
+		// Error
+		exit(EXIT_FAILURE);
+	}
+
 	exit(EXIT_SUCCESS);
 }
