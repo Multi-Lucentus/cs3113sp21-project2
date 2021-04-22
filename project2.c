45ac5cb6b0467bba76bcd554a5b9731151debd68
Parker Brandt <parker.brandt101@gmail.com>
2021-04-22 19:26:15 +0000

    Implemented readline() and split() functions - just taken from my last project

--- project2.c
+++ project2.c
@@ -79,3 +79,62 @@ int main(int argc, char** argv)
 
 	exit(EXIT_SUCCESS);
 }
+
+
+// Function Declarations
+/*
+ * Reads a line from the file pointed to by the file descriptor
+ * Returns the line read
+ */
+char* readline(int fd)
+{
+	char* buffer = (char*)malloc(64 * sizeof(char));
+	int bufCount = 0;
+
+	char c;
+	while(read(fd, &c, 1) > 0)
+	{
+		// Check if the character is newline (\n)
+		if(c != '\n')
+		{
+			buffer[bufCount] = c;
+			bufCount++;
+		}
+		else
+		{
+			buffer[bufCount] = '\0';
+			break;
+		}
+	}
+
+	return buffer;
+}
+
+/*
+ * Splits a string according to the split character parameter
+ * Returns an array of strings
+ */
+char** split(char* str, char split)
+{
+	// Count number of occurrences of the split character
+	int splitCount = 0;
+	for(int i = 0; i < strlen(string); i++)
+		if(string[i] == split)
+			splitCount++;
+
+	char** stringArray = (char**)malloc((splitCount + 1) * sizeof(char*));
+	for(int i = 0; i < (splitCount + 1); i++)
+		stringArray[i] = (char*)malloc(64 * sizeof(char));
+
+	int arrayCount = 1;
+
+	stringArray[0] = strtok(string, &split);
+
+	while(stringArray[arrayCount] != NULL)
+	{
+		stringArray[arrayCount] = strtok(NULL, &split);
+		arrayCount++;
+	}
+
+	return stringArray;
+}
